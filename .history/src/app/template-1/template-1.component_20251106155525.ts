e1553f6b94ca9172bb7a22f7d5a4f05b17aab8ea
kelliev <40643335+kelliev@users.noreply.github.com>
2025-11-12 14:31:06 -0700

    dash updates

--- .history/src/app/template-1/template-1.component_20251106155525.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-template-1',
-    imports: [],
-    templateUrl: './template-1.component.html',
-  styleUrl: './template-1.component.scss',
-  standalone: true
-})
-export class Template1Component {}
